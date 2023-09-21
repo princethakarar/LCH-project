181156ad66ab51dc67cac7e167ef7aff22709267
princethakarar <prince.thakarar40@gmail.com>
2023-09-21 23:40:54 +0530

    save changes

---
+++ Book.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Book.css">
+    <title>Books</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="row">
+            <div class="box">
+                <a href="#">
+                    <img src="images/bhagvat geeta.jpg" alt="bhagvat geeta">
+                    <p class="heading">Srimad Bhagavad Gita<br>(HINDI)</p>
+                    <p class="content">An episode recorded in the Mahabharata,<br> a Sanskrit epic poem of ancient India.</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/ramayan.jpg" alt="ramayan">
+                    <p class="heading">Ramayan (HINDI)</p>
+                    <p class="content">Along the way, it teaches Hindu life<br> lessons.</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/rich dad poor dad.jpg" alt="rich dad poor dad">
+                    <p class="heading">Rich Dad Poor Dad (HINDI)</p>
+                    <p class="content">"Rich Dad Poor Dad" by Robert Kiyosaki contrasts the financial philosophies of his<br> "rich dad" and "poor dad".</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/7 money rules.jpg" alt="7 money rules">
+                    <p class="heading">7 Money Rules for Life (R) - <br><span style="font-size:13px;">How to Take Control of Your Financial Future</span></p>
+                    <p class="content">"7 Money Rules for Life" by Mary Hunt<br> offers practical financial advice.</p>
+                </a>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="box">
+                <a href="#">
+                    <img src="images/psychology of money.jpg" alt="psychology of money">
+                    <p class="heading">The Psychology Of Money</p>
+                    <p class="content">"The Psychology of Money" by Morgan<br> Housel explores the complex relationship between human behavior and money.</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/rich dad poor dad.jpg" alt="rich dad poor dad">
+                    <p class="heading">Rich Dad Poor Dad (HINDI)</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/7 money rules.jpg" alt="7 money rules">
+                    <p class="heading">7 Money Rules for Life (R) - <br>How to Take Control of Your Financial Future</p>
+                </a>
+            </div>
+            <div class="box">
+                <a href="#">
+                    <img src="images/psychology of money.jpg" alt="psychology of money">
+                    <p class="heading">The Psychology Of Money</p>
+                </a>
+            </div>
+        </div>  
+    </main>
+    <footer></footer>
+</body>
+</html>

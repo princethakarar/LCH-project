81b6a687de33b2fc3a76901264fab795add3534b
princethakarar <prince.thakarar40@gmail.com>
2023-09-28 10:45:10 +0530

    save chnages

--- Home.php
+++ Home.php
@@ -108,25 +108,47 @@
         </div>
     </main>
     <footer>
-        <form action="submit_feedback.php" method="POST">      
-            <table>
-                <tr>
-                    <td><label for="name">Name:</label></td>
-                    <td><input type="text" id="name" name="name" required></td>
-                </tr>
-                <tr>
-                    <td><label for="email">Email:</label></td>
-                    <td><input type="email" id="email" name="email" required></td>
-                </tr>
-                <tr>
-                    <td><label for="feedback">Feedback:</label></td>
-                    <td><textarea id="feedback" name="feedback" rows="4" required></textarea></td>
-                </tr>
-                <tr>
-                    <td colspan="2"><center><button type="submit">Submit</button></center></td>
-                </tr>
-            </table>
-        </form>
+            <div class="contact">
+                <h3>Contact Us</h3>
+                <i class="fa-solid fa-phone"></i>
+                <p>9999999999</p>
+                <br>
+                <i class="fa-solid fa-envelope"></i>
+                <p>librarycommunity@gmail.com</p>
+                <br>
+                <i class="fa-solid fa-location-dot"></i>
+                <p>Atmiya University, Kalawad Road, Rajkot</p>
+            </div>
+            <div class="feedback">
+                <form action="submit_feedback.php" method="POST">      
+                    <table>
+                        <tr>
+                            <td><label for="name">Name:</label></td>
+                            <td><input type="text" id="name" name="name" placeholder="Enter Username" required></td>
+                        </tr>
+                        <tr>
+                            <td><label for="email">Email:</label></td>
+                            <td><input type="email" id="email" name="email" placeholder="Enter Email" required></td>
+                        </tr>
+                        <tr>
+                            <td><label for="feedback">Feedback:</label></td>
+                            <td><textarea id="feedback" name="feedback" rows="4" placeholder="Enter Feedback" required></textarea></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><center><input type="submit" name="sb" value="Submit" class="submit"></center></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+            <div class="time">
+                <h3>Opening Time</h3>
+                <p>Mon-Sat : 9:00-6:00</p>
+                <p>Sun : 9:00-12:00</p>
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-square-twitter"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
     </footer>
 
     <script>
